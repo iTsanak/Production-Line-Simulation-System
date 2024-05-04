0b02bea1e1b09ec3fb8c959856d151d7dc84a9e0
iTsanak <itsanachtsidis@myseneca.ca>
2024-05-04 00:33:58 -0400

    Utilities module added

---
+++ Milestone2/Utilities.h
@@ -0,0 +1,32 @@
+// Date of completion: 2023-11-21
+#ifndef SDDS_UTILITIES
+#define SDDS_UTILITIES
+#include <iostream>
+#include <string>
+#include "Station.h"
+namespace sdds
+{
+    class Utilities
+    {  
+    private:
+        size_t m_widthField{ 1 };
+        
+    public:
+        static char m_delimiter;
+        void setFieldWidth(size_t newWidth);
+        size_t getFieldWidth() const;
+        std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+        static void setDelimiter(char newDelimiter) {
+            m_delimiter = newDelimiter;
+        }
+        static char getDelimeter()
+        {
+            return m_delimiter;
+        }
+        
+
+    };
+  
+}
+#endif 
+
