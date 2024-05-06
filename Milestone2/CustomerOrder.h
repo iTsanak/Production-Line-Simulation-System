63545d6aebb66c728d254f6485cc54e9b58d9e20
iTsanak <itsanachtsidis@myseneca.ca>
2024-05-05 23:57:02 -0400

    Customer order module functional

---
+++ Milestone2/CustomerOrder.h
@@ -0,0 +1,43 @@
+// Date of completion: 2023-11-21
+#ifndef SDDS_CUSTOMERORDER_H
+#define SDDS_CUSTOMERORDER_H
+#include <iostream>
+#include "Utilities.h"
+namespace sdds
+{
+    struct Item
+    {
+        std::string m_itemName;
+        size_t m_serialNumber{ 0 };
+        bool m_isFilled{ false };
+
+        Item(const std::string& src) : m_itemName(src) {};
+    };
+
+    class CustomerOrder
+    {
+        std::string m_name{};
+        std::string m_product{};
+        size_t m_cntItem{};
+        Item** m_lstItem{};
+        
+    public:
+        static size_t m_widthField;
+        CustomerOrder() = default;
+        CustomerOrder(const std::string str);
+        CustomerOrder(const CustomerOrder& other);
+        CustomerOrder& operator=(const CustomerOrder& other) = delete;
+        CustomerOrder(CustomerOrder&& other) noexcept;
+        CustomerOrder& operator=(CustomerOrder&&) noexcept;
+        ~CustomerOrder();
+
+        bool isOrderFilled() const;
+        bool isItemFilled(const std::string& itemName) const;
+        void fillItem(Station& station, std::ostream& os);
+        void display(std::ostream& os) const;
+    };
+}
+#endif // !SDDS_CUSTOMERORDER_H
+
+
+
